fade96ed313cd0dacbbec3c5aaa59af5e00dbe7b
Eric Martindale <eric@ericmartindale.com>
2015-01-08 12:13:33 -0800

    moderately functional Soundcloud import

--- config/index.js
+++ config/index.js
@@ -7,10 +7,13 @@ module.exports = {
   service: {
     name: 'DECENTRALIZE',
     authority: 'localhost:9201',
-    namespace: 'DECENTRALIZE',
-    mission: 'talk about everything decentralized!',
+    namespace: 'decentralize',
+    mission: 'the podcast',
     description: 'The best place for learning about what\'s going on with the latest decentralized tech.',
     source: 'https://github.com/martindale/decentralize',
     points: []
+  },
+  soundcloud: {
+    clientID: '98ce66124fabdff2f33beb2f810a0a25'
   }
 }
--- decentralize.js
+++ decentralize.js
@@ -1,10 +1,23 @@
 var config = require('./config');
 var procure = require('procure');
+var async = require('async');
+var rest = require('restler');
+var _ = require('lodash');
 
 var Maki = require('maki');
-var show = procure('http://decentral.fm/shows/decentralize', function(err, data) {
-  
+var Soundcloud = require('./lib/Soundcloud');
+
+// toggle this for production vs. local version.
+/*/var base = 'http://decentral.fm/';/*/
+var base = 'http://localhost:15005/';/**/
+
+procure( base + 'shows/decentralize', function(err, show) {
+
+  // TODO: catch error
+  show = JSON.parse( show );
+
   var decentralize = new Maki( config );
+  var soundcloud = new Soundcloud( config.soundcloud );
 
   var Recording = decentralize.define('Recording', {
     attributes: {
@@ -13,11 +26,10 @@ var show = procure('http://decentral.fm/shows/decentralize', function(err, data)
       released: { type: Date , default: Date.now , required: true },
       description: { type: String }
     },
-    /*/source: 'http://decentral.fm/recordings'/*/
-    source: 'http://localhost:15005/recordings'/**/,
+    source: base + 'recordings',
     icon: 'sound'
   });
-  
+
   var Index = decentralize.define('Index', {
     name: 'Index',
     routes: { query: '/' },
@@ -30,7 +42,39 @@ var show = procure('http://decentral.fm/shows/decentralize', function(err, data)
     static: true,
     internal: true
   });
+  
+  soundcloud.get('users/decentralyze/tracks', function(err, tracks) {
+    tracks = JSON.parse( tracks ).reverse();
+    
+    async.map( tracks , function(track, done) {
+      Recording.query({ /* TODO: query by title/slug */ }, function(err, recordings) {
+        var episode = _.find( recordings , function( el ) {
+          return el.title === track.title;
+        });
+        if (episode) return done( null , episode );
 
+        rest.post( base + 'recordings', {
+          //multipart: true,
+          headers: {
+            'Accept': 'application/json'
+          },
+          data: {
+            '_show': show._id,
+            title: track.title,
+            released: Date.parse( track.created_at ),
+            description: track.description
+            //audio: rest.file('')
+          }
+        }).on('complete', function(data) {
+          console.log('done!' , data );
+          done( null , data );
+        });
+      
+      });
+    }, function(err, results) {
+    
+      decentralize.start();
 
-  decentralize.start();
+    });
+  });
 });
