c947701417c0d232ce8e1242a44d7602b5464c28
Eric Martindale <eric@ericmartindale.com>
2015-02-21 04:54:41 -0800

    plug-in the latest maki.  some styling.

--- decentralize.js
+++ decentralize.js
@@ -1,4 +1,5 @@
 var config = require('./config');
+var request = require('request');
 var procure = require('procure');
 var async = require('async');
 var rest = require('restler');
@@ -6,6 +7,7 @@ var _ = require('lodash');
 
 var Maki = require('maki');
 var Soundcloud = require('./lib/Soundcloud');
+var Form = require('form-data')
 
 // toggle this for production vs. local version.
 /*/var base = 'http://decentral.fm/';/*/
@@ -16,17 +18,22 @@ procure( base + 'shows/decentralize', function(err, show) {
 
   // TODO: catch error
   show = JSON.parse( show );
+  show.base = base;
+  show.home = home;
 
   var decentralize = new Maki( config );
   var soundcloud = new Soundcloud( config.soundcloud );
 
-  var Recording = decentralize.define('Recording', {
+  var Show = decentralize.define('Show', {
     attributes: {
-      title: { type: String },
+      title: { type: String , slug: true },
+      slug: { type: String },
       recorded: { type: Date },
       released: { type: Date , default: Date.now , required: true },
-      description: { type: String }
+      description: { type: String },
+      audio: { type: String }
     },
+    names: { get: 'item' },
     source: base + 'recordings',
     icon: 'sound'
   });
@@ -36,8 +43,8 @@ procure( base + 'shows/decentralize', function(err, show) {
     routes: { query: '/' },
     templates: { query: 'index' },
     requires: {
-      'Recording': {
-        filter: {} 
+      'Show': {
+        filter: {}
       }
     },
     static: true,
@@ -50,29 +57,28 @@ procure( base + 'shows/decentralize', function(err, show) {
     tracks = JSON.parse( tracks ).reverse();
     
     async.map( tracks , function(track, done) {
-      Recording.query({ /* TODO: query by title/slug */ }, function(err, recordings) {
+      Show.query({ /* TODO: query by title/slug */ }, function(err, recordings) {
         var episode = _.find( recordings , function( el ) {
           return el.title === track.title;
         });
         if (episode) return done( null , episode );
+        
+        var streamURL = track.stream_url + '?client_id=' + config.soundcloud.clientID;
 
-        rest.post( base + 'recordings', {
-          //multipart: true,
-          headers: {
-            'Accept': 'application/json'
-          },
-          data: {
-            '_show': show._id,
-            title: track.title,
-            released: Date.parse( track.created_at ),
-            description: track.description
-            //audio: rest.file('')
-          }
-        }).on('complete', function(data) {
-          console.log('done!' , data );
-          done( null , data );
+        console.log('downloading...')
+
+        var form = new Form();
+        form.append( '_show' , show._id );
+        form.append( 'title' , track.title );
+        form.append( 'released' , Date.parse( track.created_at ) );
+        form.append( 'description' , track.description );
+        form.append( 'audio', request.get( streamURL ) );
+        form.submit( base + 'recordings' , function(err, res) {
+          res.resume();
+          console.log('ok');
+          done();
         });
-      
+
       });
     }, function(err, results) {
     
